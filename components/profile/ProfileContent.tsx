2fb3a4531b9cadab5075b8b888acaa90ec3e6e28
garvinec <garvinechan@gmail.com>
2025-10-30 20:16:56 -0700

    bug fix

--- components/profile/ProfileContent.tsx
+++ components/profile/ProfileContent.tsx
@@ -337,7 +337,7 @@ export function ProfileContent({
                 <Button
                   type="submit"
                   className="bg-matcha-100 text-matcha-800 border border-matcha-200 hover:bg-matcha-200 hover:text-matcha-900 shadow transition-all duration-200 rounded-full px-4 py-2 font-light"
-                  onClick={handleSignOut}
+                  onClick={() => handleSignOut()}
                 >
                   Sign Out
                 </Button>
